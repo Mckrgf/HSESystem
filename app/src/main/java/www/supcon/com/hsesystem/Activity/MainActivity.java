349d236d1dddfc1d54184846bf6651178597bd99
yaobing1992 <1124507487@qq.com>
2018-04-03 17:10:20 +0800

    2

--- app/src/main/java/www/supcon/com/hsesystem/Activity/MainActivity.java
+++ app/src/main/java/www/supcon/com/hsesystem/Activity/MainActivity.java
@@ -2,18 +2,22 @@ package www.supcon.com.hsesystem.Activity;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.util.Log;
 import android.widget.Toast;
 
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.MapView;
+import com.amap.api.maps.model.BitmapDescriptorFactory;
 import com.amap.api.maps.model.LatLng;
 import com.amap.api.maps.model.Marker;
 import com.amap.api.maps.model.MarkerOptions;
 import com.amap.api.maps.model.MyLocationStyle;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -21,10 +25,10 @@ import www.supcon.com.hsesystem.Base.BaseActivity;
 import www.supcon.com.hsesystem.R;
 
 public class MainActivity extends BaseActivity {
-    private static final String TAG = "MainActivity";
     @BindView(R.id.map)
     MapView map;
     private AMap aMap;
+    private ArrayList<Marker> marks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,10 +52,24 @@ public class MainActivity extends BaseActivity {
      * 初始化任务点信息
      */
     private void initMarker() {
-        LatLng latLng = new LatLng(30.180262,120.137789);
-        final Marker marker = aMap.addMarker(new MarkerOptions().position(latLng).title("北京").snippet("DefaultMarker"));
+        LatLng latLng = new LatLng(30.180262, 120.137789);
+        Marker marker = aMap.addMarker(new MarkerOptions().position(latLng).title("北京").snippet("DefaultMarker"));
         marker.setTitle("这里有一张作业票");
         marker.setSnippet("作业票内容为动火作业");
+        marker.showInfoWindow();
+        marker.setIcon(BitmapDescriptorFactory.fromBitmap(BitmapFactory
+                .decodeResource(getResources(), R.mipmap.fire_icon)));
+
+        LatLng latLng1 = new LatLng(30.180262, 120.158726);
+        Marker marker1 = aMap.addMarker(new MarkerOptions().position(latLng1).title("北京").snippet("DefaultMarker"));
+        marker1.setTitle("这里有一个报警");
+        marker1.setSnippet("报警内容为设备损坏");
+        marker1.showInfoWindow();
+
+        marks = new ArrayList<>();
+        marks.add(marker);
+        marks.add(marker1);
+
     }
 
     /**
@@ -60,20 +78,56 @@ public class MainActivity extends BaseActivity {
     private void initMap() {
         //初始化地图控制器对象
         aMap = null;
-        if (aMap == null) {
-            aMap = map.getMap();
-        }
+        aMap = map.getMap();
         MyLocationStyle myLocationStyle;
         myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
-        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE) ;//连续定位、且将视角移动到地图中心点，定位蓝点跟随设备移动。（1秒1次定位）
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATE);//连续定位、且将视角移动到地图中心点，定位蓝点跟随设备移动。（1秒1次定位）
         myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
         aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
         aMap.getUiSettings().setMyLocationButtonEnabled(true);//设置默认定位按钮是否显示，非必需设置。
         aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
+
+        //地图点击事件
+        aMap.setOnMapClickListener(new AMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                //点击地图后隐藏marker信息
+                for (int i = 0; i < marks.size(); i++) {
+                    Marker marker = marks.get(i);
+                    if (null != marker) {
+                        if (marker.isInfoWindowShown()) {
+                            marker.hideInfoWindow();
+                        }
+                    }
+                }
+            }
+        });
+
+        //marker点击事件
+        aMap.setOnMarkerClickListener(new AMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                //点击marker,显示marker信息
+                if (null != marker) {
+                    if (!marker.isInfoWindowShown()) {
+                        marker.showInfoWindow();
+                    }
+                }
+                return true;//点击marker时不会定位到中心点
+            }
+        });
+
+        //infowindow点击事件，根据不同的任务，进入任务详情界面
+        aMap.setOnInfoWindowClickListener(new AMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+                Toast.makeText(getMyApplication(), "任务详情", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         switch (requestCode) {
             case 1:
