7f9d45eab44916ffe275de205deeb025a78d93a0
yaobing1992 <1124507487@qq.com>
2018-06-19 09:53:38 +0800

    优化代码

---
+++ app/src/main/java/www/supcon/com/hsesystem/Activity/HomeActivity.java
@@ -0,0 +1,15 @@
+package www.supcon.com.hsesystem.Activity;
+
+import android.os.Bundle;
+
+import www.supcon.com.hsesystem.Base.BaseActivity;
+import www.supcon.com.hsesystem.R;
+
+public class HomeActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/main/java/www/supcon/com/hsesystem/Fragment/MapFragment.java
@@ -0,0 +1,12 @@
+package www.supcon.com.hsesystem.Fragment;
+
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by yaobing on 2018/6/15.
+ * Description xxx
+ */
+
+public class MapFragment extends Fragment {
+}
